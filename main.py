b17fcd843b51eb2ac8b46cb42c36646edc75b6cf
44779 <root@many-wholesome.co.uk>
2024-05-16 20:33:20 +0100

    added temp background

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import tkinter as tk
+global WIDTH, HEIGHT
 
+import tkinter as tk
+from PIL import Image, ImageTk
+
+#Create Root Window
 root = tk.Tk()
 root.attributes('-fullscreen', True)
+WIDTH = root.winfo_screenwidth()
+HEIGHT = root.winfo_screenheight()
 
-quit = tk.Button(root, text="QUIT", fg="red", command=root.destroy)
-quit.place(x=10,y=10)
 
+#Load and Place Background
+img = Image.open('assets/back.jpg').resize((WIDTH, HEIGHT)) #TEMP Image (When real image is used white borders [should] disappear)
+img = ImageTk.PhotoImage(img)
+tk.Label(root, image = img).place(x=0, y=0)
+
+#Place Quit Button
+quit = tk.Button(root, text="QUIT", bg="darkred", fg = "white", command=root.destroy)
+quit.place(x=WIDTH-35,y=0) #Ugly and Hardcoded, fix later
+
+#Mainloop
 root.mainloop()

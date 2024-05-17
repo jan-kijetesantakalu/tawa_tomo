8bb41d5e8f9b107041ff31f1bf5dc4332793f37b
Phoebe Martin <20PMartin@patesgs.org>
2024-05-17 10:22:38 +0100

    Added Temporary Images

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ global WIDTH, HEIGHT
 import tkinter as tk
 from PIL import Image, ImageTk
 
+
+
 #Create Root Window
 root = tk.Tk()
 root.attributes('-fullscreen', True)
@@ -15,9 +17,18 @@ img = Image.open('assets/back.jpg').resize((WIDTH, HEIGHT)) #TEMP Image (When re
 img = ImageTk.PhotoImage(img)
 tk.Label(root, image = img).place(x=0, y=0)
 
+
 #Place Quit Button
 quit = tk.Button(root, text="QUIT", bg="darkred", fg = "white", command=root.destroy)
-quit.place(x=WIDTH-35,y=0) #Ugly and Hardcoded, fix later
+quit.place(x=WIDTH-500,y=0) #Ugly and Hardcoded, fix later
+
+
+# Adding stoof
+lava = Image.open('assets/lavaLamp.jpg')
+lava = ImageTk.PhotoImage(lava)
+tk.Label(root, image = lava).place(x=100, y=100)
+
+
 
 #Mainloop
 root.mainloop()

08c035e3d1eb842b22a632bb22f163ce64dcb085
Phoebe Martin <20PMartin@patesgs.org>
2024-05-17 10:58:09 +0100

    .

--- main.py
+++ main.py
@@ -20,13 +20,21 @@ tk.Label(root, image = img).place(x=0, y=0)
 
 #Place Quit Button
 quit = tk.Button(root, text="QUIT", bg="darkred", fg = "white", command=root.destroy)
-quit.place(x=WIDTH-500,y=0) #Ugly and Hardcoded, fix later
+quit.place(x = int(WIDTH/2), y = 0) #Ugly and Hardcoded, fix later
 
 
 # Adding stoof
-lava = Image.open('assets/lavaLamp.jpg')
+lava = Image.open('assets/lavaLamp.jpg').resize((int(WIDTH/10), int(HEIGHT/10)))
 lava = ImageTk.PhotoImage(lava)
-tk.Label(root, image = lava).place(x=100, y=100)
+tk.Label(root, image = lava).place(x = int(WIDTH/4*1.35),y = (int(HEIGHT/2*0.9)))
+
+lamp = Image.open('assets/lamp.jpg').resize((int(WIDTH/10), int(HEIGHT/10)))
+lamp = ImageTk.PhotoImage(lamp)
+tk.Label(root, image = lamp).place(x = int(WIDTH/3*1.8),y = (int(HEIGHT/2*0.9)))
+
+ort = Image.open('assets/Ort.jpg').resize((int(WIDTH/10), int(HEIGHT/10)))
+ort = ImageTk.PhotoImage(ort)
+tk.Label(root, image = ort).place(x = int(WIDTH/4*2.5),y = (int(HEIGHT/2*1.4)))
 
 
 

c8e6a25f687de2025097ce5a8d63e88c7097107f
44779 <root@many-wholesome.co.uk>
2024-05-24 18:44:08 +0100

    Odd Aspects

--- main.py
+++ main.py
@@ -143,6 +143,14 @@ root.bind("<KeyPress>", handle_keypress)
 WIDTH = root.winfo_screenwidth()
 HEIGHT = root.winfo_screenheight()
 
+if WIDTH/HEIGHT - 16/9 > 0.02:
+    #Screen is wider than expected
+    WIDTH = HEIGHT* 16/9
+elif WIDTH/HEIGHT - 16/9 < 0.2:
+    #Screen is taller than expected
+    HEIGHT = WIDTH * 9/16
+
+
 #Initialise Canvas
 
 canvas = Image.new(mode= "RGBA", size=(596,336))
